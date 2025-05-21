cd3baec3bda5ab07e7a9278fc84fdca476cc061c
Temp User <temp@example.com>
2025-05-21 06:47:58 +0200

    Poprawka kafelków kontaktowych - równy rozmiar i ucięcie tekstu

--- src/App.tsx
+++ src/App.tsx
@@ -78,14 +78,14 @@ const App = () => {
                   <line x1="2" y1="19" x2="9" y2="13" />
                   <line x1="22" y1="19" x2="15" y2="13" />
                 </svg>
-                <span>piotr.peszkoo@gmail.com</span>
+                <span className="truncate">piotr.peszkoo@gmail.com</span>
               </a>
               
               <div className="contact-item">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
                   <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z" />
                 </svg>
-                <span>+48 535272451</span>
+                <span className="truncate">+48 535272451</span>
               </div>
               
               <div className="contact-item">
@@ -93,7 +93,7 @@ const App = () => {
                   <path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span>Kukułcza 26, 43-190 Mikołów, Poland</span>
+                <span className="truncate">Kukułcza 26, 43-190 Mikołów, Poland</span>
               </div>
             </motion.div>
           </header>
